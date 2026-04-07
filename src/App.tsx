2692d3a2594081bfe99fd0165f12195a69d9d9fb
Farhan <farhan@aptlylabs.com>
2026-04-07 12:49:58 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
